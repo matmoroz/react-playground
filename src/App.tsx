6b935f9e2fae330cf75c3a52f5d9026a7b588e80
matmoroz <matmoroz@gmail.com>
2021-11-21 17:51:32 +0100

    playareound with app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Moroz uczy Moroza!</p>
+        <p>Test Paragrafu !</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
+        <span>
+          <button type="button" onClick={() => setCount((count) => count + -1)}>
+            count is: {count}
+          </button>
+        </span>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
@@ -27,7 +33,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +45,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
